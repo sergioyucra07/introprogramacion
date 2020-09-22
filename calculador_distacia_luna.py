4edaacff8aa92bc86af753f86b5a7446fb162cd5
sergio yucra <sergioyeyiti@gmail.com>
2020-09-22 13:38:01 -0400

    Leer distancia recorrida del, teclado

--- calculador_distacia_luna.py
+++ calculador_distacia_luna.py
@@ -1,4 +1,4 @@
 distancia_tierra_luna = 384400
-distancia_recorrida= 50000
+distancia_recorrida= int(input("Por favor, ingrese distancia recorrida:"))
 distancia_restante = distancia_tierra_luna - distancia_recorrida
-print(distancia_restante)
+print("Distancia restante:", distancia_restante)
